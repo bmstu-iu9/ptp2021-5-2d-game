366e3d2f0aae81f8836c6061ea388847cac88ae5
stasstaf <stasstaf2002@gmail.com>
2021-08-29 22:28:06 +0300

    Небольшие правки

--- js/components/flame_render.js
+++ js/components/flame_render.js
@@ -4,10 +4,9 @@ import AssetsManager from "../core/assets_manager.js";
 export {FlameRender}
 
 class FlameRender extends Component {
-    constructor(owner, animationAtlasName, flameHeight=120, flameWidth=70) {
+    constructor(owner, animationAtlasName, flameHeight=70, flameWidth=70) {
         super("FlameRender", owner);
         this.atlas = AssetsManager.textures[animationAtlasName]
-        this.body = owner.body
         this.cellIndex = 0
         this.flameHeight = flameHeight
         this.flameWidth = flameWidth
@@ -15,10 +14,10 @@ class FlameRender extends Component {
     preRender(ctx) {
         super.preRender(ctx)
         ctx.save()
-        ctx.translate(this.body.centerX, this.body.centerY)
+        ctx.translate(this.owner.body.centerX, this.owner.body.centerY)
         ctx.rotate(-90*Math.PI/180)
         let cell = this.atlas.cells[this.cellIndex]
-        ctx.drawImage(this.atlas.image, cell.x, cell.y, cell.w, cell.h,  -this.flameHeight+this.body.width/2, -this.flameWidth/2,
+        ctx.drawImage(this.atlas.image, cell.x, cell.y, cell.w, cell.h,  -this.flameHeight, -this.flameWidth/2,
             this.flameHeight, this.flameWidth)
         ctx.restore();
         this.cellIndex = (this.cellIndex + 1)%this.atlas.cells.length;
